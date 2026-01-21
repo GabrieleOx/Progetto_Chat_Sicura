42ba655845d36cd2b11e573d1cb8d4a197e4086e
fede285 <149192957+fede285@users.noreply.github.com>
2026-01-21 14:22:30 +0100

    Update cli.py
    box box

--- temp_fede/cli.py
+++ temp_fede/cli.py
@@ -4,9 +4,23 @@ from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Input, Static
 from textual.containers import Vertical
 from chat import ChatWindow
+from colorama import Fore, init
+import os
+import signal
+
+# Ignora Ctrl+C, così non chiude l'app
+signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+
+def cls(): 
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+init(autoreset=True)
 
 HOST = "localhost"
 PORT = 3000
+
+COLORS = [Fore.CYAN, Fore.GREEN]
 
 class ChatApp(App):
     CSS = "Static { height: 1fr; }"
@@ -24,7 +38,9 @@ class ChatApp(App):
     def on_mount(self):
         self.sock = socket.socket()
         self.sock.connect((HOST, PORT))
+        self.client_id = None
         self.chat_windows = {}  # chat_id -> ChatWindow
+        self.chat_colors = {}   # chat_id -> {client_id: colore}
         threading.Thread(target=self.listen, daemon=True).start()
 
     def listen(self):
@@ -45,46 +61,43 @@ class ChatApp(App):
         self.call_from_thread(self.exit)
 
     def handle(self, msg):
+        if msg.startswith("ID;"):
+        
+            _, cid = msg.split(";", 1)
+            self.client_id = cid
+            self.main_display_text += f"Il tuo ID: {cid}\n"
+            self.main_display.update(self.main_display_text)
 
-     if msg.startswith("ID;"):
-        _, cid = msg.split(";", 1)
-        self.client_id = cid
-        self.main_display_text += f"Il tuo ID: {cid}\n"
-        self.main_display.update(self.main_display_text)
+        elif msg.startswith("USERS;"):
+            cls()
+            self.main_display_text = "Comandi: LIST | CHAT <id>\n"
+            users = msg.split(";")[1:]
+            self.main_display_text += "Utenti connessi:\n"
+            for u in users:
+                self.main_display_text += f"- {u}\n"
+            self.main_display.update(self.main_display_text)
 
-     elif msg.startswith("USERS;"):
-        users = msg.split(";")[1:]
-        if users:
-            self.main_display_text += "Utenti connessi:\n" + "\n".join(users) + "\n"
-        else:
-            self.main_display_text += "Utenti connessi: Nessun altro utente\n"
-        self.main_display.update(self.main_display_text)
-
-     elif msg.startswith("START;"):
-        _, chat_id, peer = msg.split(";", 2)
-
-        if chat_id not in self.chat_windows:
-            chat = ChatWindow(chat_id, peer, self.sock)
-            self.chat_windows[chat_id] = chat
-            self.mount(chat)   
-
-     elif msg.startswith("MSG;"):
-        _, chat_id, sender, text = msg.split(";", 3)
-
-        if chat_id not in self.chat_windows:
-            chat = ChatWindow(chat_id, sender, self.sock)
-            self.chat_windows[chat_id] = chat
-            self.mount(chat)
-
-        self.chat_windows[chat_id].receive_message(sender, text)
-
-     elif msg.startswith("CLOSE;"):
-        _, chat_id = msg.split(";", 1)
-        if chat_id in self.chat_windows:
-            self.chat_windows[chat_id].remove()
-            del self.chat_windows[chat_id]
+        elif msg.startswith("START;"):
+            _, chat_id, peer_id = msg.split(";", 2)
+            if chat_id not in self.chat_windows:
+                chat = ChatWindow(chat_id, peer_id, self.sock)
+                chat._client_id = self.client_id
+                self.chat_windows[chat_id] = chat
+                self.mount(chat)
+                chat.input.focus()
 
 
+
+        elif msg.startswith("MSG;"):
+            _, chat_id, sender_id, text = msg.split(";", 3)
+            if chat_id in self.chat_windows:
+                self.chat_windows[chat_id].receive_message(sender_id, text)
+
+        elif msg.startswith("CLOSE;"):
+            _, chat_id = msg.split(";", 1)
+            if chat_id in self.chat_windows:
+                self.chat_windows[chat_id].remove()
+                del self.chat_windows[chat_id]
 
     def on_input_submitted(self, event):
         text = event.value.strip()
@@ -100,7 +113,7 @@ class ChatApp(App):
             parts = text.split()
             if len(parts) == 2:
                 try:
-                    self.sock.sendall(f"CHAT {parts[1]}\n".encode())
+                    self.sock.sendall(f"CHAT;{parts[1]}\n".encode())
                 except:
                     self.main_display_text += f"Errore: impossibile avviare chat con {parts[1]}\n"
                     self.main_display.update(self.main_display_text)
