75b91f38d5f018dbe46f26393ed08c442ddc609c
GabrieleOx <ossolagabriele@gmail.com>
2026-01-22 21:33:13 +0100

    Unione server con compatibilità: Picle invece di stringhe con ; Aggiunta parte REG/LOG

---
+++ temp_gabri/cli_to_rescue.py
@@ -0,0 +1,263 @@
+from colorama import Fore, init #pip install colorama
+import os
+import pickle as pk #per gestire un dizionario codificato in bytes
+from getpass import getpass
+import socket as sk
+import threading as th
+
+#pip install pycryptodome: libreria per SECURITY
+from Crypto.PublicKey import RSA #docs RSA: https://pycryptodome.readthedocs.io/en/latest/src/public_key/rsa.html
+from Crypto.Hash import SHA256
+from Crypto.Util.number import long_to_bytes, bytes_to_long
+from Crypto.Cipher import AES #docs per la mode GCM: https://pycryptodome.readthedocs.io/en/latest/src/cipher/modern.html#gcm-mode
+from Crypto.Random import get_random_bytes
+
+#pip install textual: libreria per UI
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer, Input, Static
+from textual.containers import Vertical
+
+#we'll see
+import base64
+import json
+
+ADDRESS = ("localhost", 3000)
+password = "ciaooo122"
+
+class ChatApp(App):
+    CSS = "Static { height: 1fr; }"
+
+    # ================= AES =================
+    def simmetriCryption(self, plainText, key):
+        header = b"header"
+        data = plainText.encode()
+        cipher = AES.new(key, AES.MODE_GCM)
+        cipher.update(header)
+        ciphertext, tag = cipher.encrypt_and_digest(data)
+
+        json_k = ['nonce', 'header', 'ciphertext', 'tag']
+        json_v = [base64.b64encode(x).decode('utf-8') for x in (cipher.nonce, header, ciphertext, tag)]
+        return json.dumps(dict(zip(json_k, json_v)))
+
+    def simmetricDecryption(self, cipherText, key):
+        try:
+            b64 = json.loads(cipherText)
+            json_k = ['nonce', 'header', 'ciphertext', 'tag']
+            jv = {k: base64.b64decode(b64[k]) for k in json_k}
+            cipher = AES.new(key, AES.MODE_GCM, nonce=jv['nonce'])
+            cipher.update(jv['header'])
+            plaintext = cipher.decrypt_and_verify(jv['ciphertext'], jv['tag'])
+            return plaintext.decode('utf-8')
+        except (ValueError, KeyError):
+            return "Incorrect decryption"
+
+    # ================= RSA =================
+    def cryptWithPublic(self, data: bytes, path: str):
+        public_key_data = open(path, "rb").read()
+        pub = RSA.import_key(public_key_data)
+        m = bytes_to_long(data)
+        c = pow(m, pub.e, pub.n)
+        return long_to_bytes(c)
+
+    def decryptWithPrivate(self, cipherText: bytes, path: str):
+        c = bytes_to_long(cipherText)
+        private_key_data = open(path, "rb").read()
+        pvt = RSA.import_key(private_key_data, passphrase=password)
+        m = pow(c, pvt.d, pvt.n)
+        return long_to_bytes(m)
+
+    # ================= TUI =================
+    def compose(self) -> ComposeResult: #Prima funzione che si avvia e costruisce il Textual
+        yield Header()
+        with Vertical():
+            self.output = Static("", markup=True)
+            yield self.output
+            self.input = Input(placeholder="> ")
+            yield self.input
+        yield Footer()
+
+    def on_mount(self): #Funzione "main"
+
+        #connessione al server
+        self.sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+        try:
+            self.sock.connect(ADDRESS)
+        except:
+            print(Fore.RED + f"Connessione a server remoto {ADDRESS} fallita.")
+            exit(0)
+        #connessione riuscita
+
+        self.client_username = None
+        self.mode = "menu"
+        self.current_chat = None
+
+        self.users = []  # utenti connessi ??
+        self.chats = {}  # chat_id -> {peer, messages[], sessionKey}
+
+        self.render_menu()
+        th.Thread(target=self.listen, daemon=True).start()
+
+    # ================= NETWORK =================
+    def listen(self):
+        buffer = ""
+        while True:
+            try:
+                data = self.sock.recv(8192)
+                if not data:
+                    break
+                buffer += data.decode()
+                while "\n" in buffer:
+                    line, buffer = buffer.split("\n", 1)
+                    self.call_from_thread(self.handle, line.strip())
+            except:
+                break
+
+    def handle(self, msg): #dopo l'avvio del thread (on_mount)
+        if msg.startswith("ID;"):
+            self.client_username = msg.split(";", 1)[1]
+            self.render_menu()
+
+        elif msg.startswith("USERS;"):
+            self.users = [u for u in msg.split(";")[1:] if u]
+            self.render_menu() #tolto show_users: inutilizzato
+
+        elif msg.startswith("START;"):
+            _, chat_id, peer, encoded = msg.split(";", 3)
+            encrypted = base64.b64decode(encoded)
+            sessionKey = self.decryptWithPrivate(
+                encrypted,
+                "private_corrente.der"
+            )
+            if chat_id not in self.chats:
+                self.chats[chat_id] = {"peer": peer, "messages": [], "sessionKey": sessionKey}
+            self.render_menu()
+
+        elif msg.startswith("MSG;"):
+            _, chat_id, sender, text = msg.split(";", 3)
+            if chat_id in self.chats:
+                name = "Tu" if sender == self.client_username else sender
+                color = "cyan" if sender == self.client_username else "green"
+                self.chats[chat_id]["messages"].append(f"[{color}]{name}: {text}[/]")
+                if self.mode == "chat" and self.current_chat == chat_id:
+                    self.render_chat(chat_id)
+
+        elif msg.startswith("CLOSE;"):
+            _, chat_id = msg.split(";", 1)
+            self.chats.pop(chat_id, None)
+            if self.current_chat == chat_id:
+                self.mode = "menu"
+                self.current_chat = None
+                self.render_menu()
+
+    # ================= UI =================
+    def render_menu(self):
+        self.mode = "menu"
+
+        text = "[yellow]=== COMANDI ===[/]\n"
+        text += "CHAT <id>           → avvia chat\n"
+        text += "OPEN <chat_id>      → entra in chat\n"
+        text += "CLOSE <chat_id>     → chiudi chat\n"
+        text += "P.S: ESATTAMENTE UNO SPAZIO TRA KEYWORD E ID \n\n"
+        if self.client_username:
+            text += f"[yellow]Il tuo ID:[/] {self.client_username}\n\n"
+
+        text += "\n[yellow]=== UTENTI CONNESSI ===[/]\n"
+        if self.users:
+            for u in self.users:
+                text += f"- {u}\n"
+        else:
+            text += "Nessun altro utente\n"
+        text += "\n"
+
+        text += "[yellow]=== CHAT ATTIVE ===[/]\n"
+        if self.chats:
+            for cid, c in self.chats.items():
+                text += f"- {cid} (con {c['peer']})\n"
+        else:
+            text += "Nessuna chat attiva\n"
+
+        self.output.update(text)
+
+    def render_chat(self, chat_id):
+        chat = self.chats[chat_id]
+        key = chat["sessionKey"]
+
+        text = f"[yellow]Chat con {chat['peer']} [{chat_id}][/]\n"
+        text += "-" * 40 + "\n"
+
+        for msg in chat["messages"]:
+            if msg.startswith("[cyan]Tu: "):
+                payload = msg.replace("[cyan]Tu: ", "").replace("[/]", "")
+                plain = self.simmetricDecryption(payload, key)
+                text += f"[cyan]Tu: {plain}[/]\n"
+            else:
+                sender, payload = msg.split(": ", 1)
+                payload = payload.replace("[/]", "")
+                plain = self.simmetricDecryption(payload, key)
+                text += f"{sender}: {plain}\n"
+
+        text += "\n[yellow]/exit[/] → torna al menu\n"
+        text += "[yellow]/close[/] → chiudi chat\n"
+
+        self.output.update(text)
+
+    # ================= INPUT =================
+    def on_input_submitted(self, event):
+        text = event.value.strip()
+        event.input.value = ""
+
+        if self.mode == "menu":
+            self.handle_menu_input(text)
+        else:
+            self.handle_chat_input(text)
+
+    def handle_menu_input(self, text):
+        if text.startswith("CHAT "):
+            _, uid = text.split(" ", 1)
+            sessionKey = get_random_bytes(32)
+            encrypted = self.cryptWithPublic(
+                sessionKey,
+                "public_corrente.der"
+            )
+            encoded = base64.b64encode(encrypted).decode()
+            self.sock.sendall(f"CHAT;{uid};{encoded}\n".encode())
+
+        elif text.startswith("OPEN "):
+            _, chat_id = text.split(" ", 1)
+            if chat_id in self.chats:
+                self.mode = "chat"
+                self.current_chat = chat_id
+                self.render_chat(chat_id)
+
+        elif text.startswith("CLOSE "):
+            _, chat_id = text.split(" ", 1)
+            if chat_id in self.chats:
+                self.sock.sendall(f"CLOSE;{chat_id}\n".encode())
+                self.chats.pop(chat_id, None)
+                self.render_menu()
+
+    def handle_chat_input(self, text):
+        chat_id = self.current_chat
+        key = self.chats[chat_id]["sessionKey"]
+
+        if text == "/exit":
+            self.mode = "menu"
+            self.current_chat = None
+            self.render_menu()
+
+        elif text == "/close":
+            self.sock.sendall(f"CLOSE;{chat_id}\n".encode())
+            self.chats.pop(chat_id, None)
+            self.mode = "menu"
+            self.current_chat = None
+            self.render_menu()
+
+        else:
+            cipher = self.simmetriCryption(text, key)
+            self.chats[chat_id]["messages"].append(f"[cyan]Tu: {cipher}[/]")
+            self.sock.sendall(f"MSG;{chat_id};{cipher}\n".encode())
+            self.render_chat(chat_id)
+
+
+if __name__ == "__main__":
+    ChatApp().run()
---
+++ temp_gabri/secure_server.py
@@ -0,0 +1,298 @@
+import uuid
+from colorama import Fore, init #pip install colorama 
+import pickle as pk
+import socket as sk
+import mariadb as db #pip install mariadb
+import threading as th
+
+ADDRESS = ("localhost", 3000)
+
+client_loggati: dict[str, sk.socket] = {} # username : connessione al client
+chats = {}     # chat_id -> (id1, id2)
+lock = th.Lock()
+
+db_params = {
+    "user" : "progetto_chat",
+    "password" : "password-server", #queste credenziali sono per il mio database locale...
+    "host" : "localhost",
+    "database" : "progetto_chat_sicura"
+}
+
+
+def broadcast_users():
+    with lock:
+        for username, conn in client_loggati.items():
+            others = [x for x in client_loggati if x != username]
+            conn.send(pk.dumps(("U", others))) #USERS = U
+
+
+def start_chat(a, b, sessionKey):
+    """
+    Docstring for start_chat
+    
+    :param a: ID del primo client
+    :param b: ID del secondo client
+    :param sessionKey: chiave di sessione simmetrica
+    """
+
+    with lock:
+        if a not in client_loggati or b not in client_loggati:
+            return
+
+        chat_id = str(uuid.uuid4())[:8]
+        chats[chat_id] = (a, b)
+        
+
+        client_loggati[a].send(pk.dumps(("O", [chat_id, b, sessionKey]))) # START = O like OPEN
+        client_loggati[b].send(pk.dumps(("O", [chat_id, a, sessionKey])))
+
+
+def relay(chat_id, sender, text):
+    """
+    Docstring for relay
+    
+    :param chat_id: id della chat
+    :param sender: id di chi manda il messaggio
+    :param text: messaggio
+    """
+
+    with lock:
+        if chat_id not in chats:
+            return
+        a, b = chats[chat_id]
+        target = b if sender == a else a
+        if target in client_loggati:
+            client_loggati[target].send(pk.dumps(("M", [chat_id, sender, text]))) # MSG = M
+
+
+def close_chat(chat_id):
+    with lock:
+        if chat_id not in chats:
+            return
+        a, b = chats.pop(chat_id)
+        if a in client_loggati:
+            client_loggati[a].send(pk.dumps(("C", chat_id))) # CLOSE = C
+        if b in client_loggati:
+            client_loggati[b].send(pk.dumps(("C", chat_id)))
+
+
+
+
+
+
+
+
+
+
+
+
+#new \/
+
+def registration(data: dict) -> int:
+    """
+    Docstring for registration
+    
+    :param data: dati da inserire nel db
+    :type data: dict
+    :return: 0: tutto ok, 1: errore utente già presente, 2: errore nell'insermento dei dati
+    :rtype: int
+    """
+
+    utente = {str(key) : data[str(key)] for key in data.keys() if str(key) not in ["pbkey", "pvkey"]}
+    chiavi = {str(key) : data[str(key)] for key in data.keys() if str(key) in ["pvkey", "pbkey", "username"]}
+
+    username = data["username"]
+
+    with db.connect(**db_params) as conn:
+        with conn.cursor() as cur:
+
+            try:
+                #controllo che l'utente non esista già
+                cur.execute("SELECT username FROM utente")
+                utenti = [row[0] for row in cur.fetchall()]
+                if username in utenti:
+                    return 1
+                
+                #inserisco i dati
+                cur.execute(f"INSERT INTO utente ({",".join(str(key) for key in utente.keys())}) VALUES ({",".join("?" for i in range(len(utente)))})", tuple(utente[key] for key in utente.keys()))
+
+                """if "nome" in data.keys():
+                    if "cognome" in data.keys():
+                        cur.execute("INSERT INTO utente (username, password, nome, cognome) VALUES (?, ?, ?, ?)", (username, password_hash, data["nome"], data["cognome"]))
+                    else:
+                        cur.execute("INSERT INTO utente (username, password, nome) VALUES (?, ?, ?)", (username, password_hash, data["nome"]))
+                else:
+                    if "cognome" in data.keys():
+                        cur.execute("INSERT INTO utente (username, password, cognome) VALUES (?, ?, ?)", (username, password_hash, data["cognome"]))
+                    else:
+                        cur.execute("INSERT INTO utente (username, password) VALUES (?, ?)", (username, password_hash))
+                conn.commit()"""
+                #inserisco le chiavi
+                cur.execute(f"INSERT INTO user_key ({",".join(str(key) for key in chiavi.keys())}) VALUES ({",".join("?" for i in range(len(chiavi)))})", tuple(chiavi[key] for key in chiavi.keys()))
+                conn.commit()
+            except:
+                cur.execute("DELETE FROM utente WHERE username = ?", (username,))
+                conn.commit()
+                return 2
+    
+    return 0
+
+def access(data: dict, conn_client: sk.socket) -> tuple[int] | tuple[int, bytes]:
+    """
+    Docstring for access
+    
+    :param data: dati da verificare col db
+    :type data: dict
+    :return: 0: loggato correttamente, 1: errore utente inesistente, 2: errore utente già loggato, 3: errore password errata
+    :rtype: int
+    """
+    global client_loggati
+    username = data["username"]
+    password_hash = data["password_hash"]
+
+    with db.connect(**db_params) as conn:
+        with conn.cursor() as cur:
+
+            try:
+                #controllo che l'utente esista
+                cur.execute("SELECT username, password FROM utente")
+                utenti_pass = {row[0] : row[1] for row in cur.fetchall()}
+
+                if username not in utenti_pass.keys():
+                    return (1,)
+                
+                if username in client_loggati.keys():
+                    return (2,)
+                
+                if password_hash == utenti_pass[username]:
+                    client_loggati[username] = conn_client
+                    cur.execute("SELECT k.pvkey FROM user_key k JOIN utente u ON u.username = k.username WHERE u.username = ?", (username,))
+                    conn.commit()
+                    pvkey: bytes = cur.fetchone()[0]
+                    return (0,pvkey)
+                else: return (3,)
+            except:
+                return (4,)
+    return (4,)
+
+
+
+def request_key(username: str) -> tuple[int] | tuple[int, bytes]:
+    with db.connect(**db_params) as conn:
+        with conn.cursor() as cur:
+
+            try:
+                #controllo che l'utente esista
+                cur.execute("SELECT username, password FROM utente")
+                utenti_pass = {row[0] : row[1] for row in cur.fetchall()}
+
+                if username not in utenti_pass.keys():
+                    return (1,)
+                
+                if username not in client_loggati.keys():
+                    return (2,)
+
+                cur.execute("SELECT pbkey FROM user_key WHERE username = ?", (username,))
+                conn.commit()
+                
+                pbkey: bytes = cur.fetchone()[0]
+                return (0, pbkey)
+            except:
+                return (3,)
+    
+    return (3,)
+
+
+def loggato(username: str, conn: sk.socket):
+    #da fare con pickle \/
+        while True:
+            data = conn.recv(8192)
+            if not data:
+                break
+            
+            recived = pk.loads(data)
+            match recived[0]:
+                #già da loggato \/
+                case "U":
+                    utenti_online = [str(u) for u in client_loggati.keys() if str(u) != recived[1]]
+                    conn.send(pk.dumps(("U", utenti_online)))
+
+                case "E":
+                    username_remove = recived[1]
+                    client_loggati.pop(username_remove)
+
+                case "K":
+                    ex = request_key(recived[1])
+                    conn.send(pk.dumps(("K", ex)))
+                    break #esce dall "modalità loggato"
+
+                case "S": start_chat(username, recived[1][0],recived[1][1]) #CHAT = S
+
+                case "M": relay(recived[1][0], username, recived[1][1]) #MSG = M
+
+                case "C": close_chat(recived[1]) #CLOSE = C
+
+
+
+
+
+
+
+
+
+
+def handle(conn: sk.socket, addr):
+    print(Fore.GREEN + f"Connesso {addr}")
+
+    while True:
+        try:
+            recived = conn.recv(3072) #valore alto per tranquillità
+            if not recived:
+                break
+
+            recived = pk.loads(recived)
+
+            match recived[0]: #smista le richieste
+                case "R": 
+                    code = registration(recived[1])
+                    conn.send(pk.dumps(("R", code)))
+                case "L":
+                    ex = access(recived[1], conn)            
+                    data_dict: dict[str, int | bytes] = {
+                        "code" : ex[0]
+                    }
+                    if ex[0] == 0:
+                        client_loggati[recived[1]["username"]] = conn
+                    if len(ex) == 2:
+                        data_dict["private_key"] = ex[1]
+                    conn.send(pk.dumps(("L", data_dict)))
+                    if ex[0] == 0:
+                        loggato(recived[1]["username"], conn)
+        except:
+            break
+    
+    conn.close() # Disconnessione sicura
+    broadcast_users()
+    print(Fore.RED + f"Client {addr} disconnesso...")
+
+
+def main():
+    server = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+    server.setsockopt(sk.SOL_SOCKET, sk.SO_REUSEADDR, 1)
+
+    try:
+        server.bind(ADDRESS)
+        server.listen(4)
+    except:
+        print(Fore.RED + f"Associazione del server all'indirizzo {ADDRESS} fallita.")
+        exit(0)
+    
+    print(Fore.BLUE + f"Server online all'indirizzo: {ADDRESS}")
+
+    while True:
+        client, indirizzo = server.accept()
+        th.Thread(target=handle, args=(client, indirizzo), daemon=True).start()
+
+
+if __name__ == "__main__":
+    main()
