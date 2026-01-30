3ec1e857c9fc3a991d1520e17feed37076a2978b
MAF3X <matteograncan@gmail.com>
2026-01-30 12:30:09 +0100

    aggiunta controllo nel file color_picker.py

--- color_picker.py
+++ color_picker.py
@@ -1,13 +1,24 @@
 from tkinter import colorchooser
 from tkinter import Tk
 
-root = Tk()
-root.withdraw()  # Nasconde la finestra principale
+def hex_to_rgb(hex_color):
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
 
-color = colorchooser.askcolor(title="Scegli un colore")[0]  # Prende solo RGB
+root = Tk()
+root.withdraw()  
+
+color = colorchooser.askcolor(title="Scegli un colore")[0] 
 
 if color:
-    print(f"Colore scelto (RGB): {color}")  # Es. (255, 0, 0)
+    print(f"Colore scelto (RGB): {color}")
     print(f"Hex: #{color[0]:02x}{color[1]:02x}{color[2]:02x}".upper())
 else:
     print("Nessun colore selezionato.")
+
+rgb=hex_to_rgb(f"{color[0]:02x}{color[1]:02x}{color[2]:02x}")
+luminanza = 0.2126 * rgb[0] + 0.7152 * rgb[1] + 0.0722 * rgb[2]
+if(luminanza>=100):
+    print("il colore è chiaro")
+else:
+    print("il colore è scuro")
